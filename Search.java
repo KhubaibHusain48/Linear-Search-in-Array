642555449d4b4075fd4d13bc3453410172c66de0
Khubaib Husain <khubaibhusain45@gmail.com>
2025-06-21 13:20:47 +0500

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arrays = {1, 2, 3, 4, 5, 1, 2, 4, 2, 3};
+        System.out.print("Enter Element to find it: ");
+        int target = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < arrays.length; i++) {
+            if (target == arrays[i]) {
+                count++;
+            }
+        }
+        if (count > 0) {
+            System.out.println("YES! Element Found");
+            System.out.println("Element found " + count + " times");
+        } else
+            System.out.println("NO! Element Not Found");
+    }
+}
